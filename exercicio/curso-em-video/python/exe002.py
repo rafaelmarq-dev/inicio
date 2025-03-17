58503077c3e379f2daa8f6f9f30911fd8a434b51
rafaelmarq-dev <rafaeldelima600@gmail.com>
2025-03-16 22:53:31 -0300

    novo arquivo

---
+++ exercicio/curso-em-video/python/exe002.py
@@ -0,0 +1,3 @@
+#DESAFIO 02 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+nome = str(input("Qual é o seu nome?\n -  "))
+print(f"Seja muito bem vindo, {nome}!")
